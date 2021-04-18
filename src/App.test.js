c1a8286743276620cac640a0dab80e851afec10a
Kristian Sigston <kristians@reallyenglish.com>
2021-04-18 21:32:23 +0100

    feat(tseting): initial setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
+import ReactDOM from 'react-dom'
+import React from 'react'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  render(<App />)
+  const linkElement = screen.getByText(/hi there/i)
+  expect(linkElement).toBeInTheDocument()
+})
+test('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+  expect(div.innerHTML).toContain('Hi There')
+  ReactDOM.unmountComponentAtNode(div)
+})
